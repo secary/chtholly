0f78a299a89d5dcd5b6fe316cf9e904cca79b310
secary2010 <secary2010@outlook.com>
2024-06-12 12:32:35 +0930

    Update files

--- 7107datatming/dt_practice/dtw1/prac_1.R
+++ 7107datatming/dt_practice/dtw1/prac_1.R
@@ -2,8 +2,6 @@ install.packages("tidyverse")
 library(tidyverse)
 data("mpg")
 
-install.packages("nycflights")
-library(nycflights13)
 
 pacman::p_load(nycflights13)
 flights
@@ -11,3 +9,5 @@ flights
 filter(flights,month == 1,day == 1)
 flights %>% filter(carrier == "AA",month == 12, day == 5) %>%select(year,flight,dep_delay)
 my_flight <- flights %>% filter(carrier == "AA",month == 12, day == 5) %>%select(year,flight,dep_delay)
+my_flight <- select(flights,contains("time"))
+delay <- my_flight %>% mutate(delay = dep_time - sched_dep_time)
