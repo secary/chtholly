cee1a8c22df63a6ef681092c7bc175f2e96b8c36
Secary M <secary2010@outlook.com>
2024-07-02 22:57:18 +0930

    finished q2

--- 7107datatming/dt_assignments/dta2/dta2.R
+++ 7107datatming/dt_assignments/dta2/dta2.R
@@ -1,6 +1,24 @@
 library(tidyverse)
 library(inspectdf)
 
+#q1
 ysn = 1942340
 filenum <- (ysn+1) %% 3
 arch0 <- read.csv('./data/archery_0.csv')
+head(arch0,10)
+
+#q2
+#q2(a)
+arch0<-mutate(arch0,Archer = (1:555))
+arch0<-rename(arch0,ID = Archer)
+
+#q2(b)
+arch0<-mutate(arch0,XP = dmy('15-06-2024') - dmy(Commenced))
+arch0<-relocate(arch0,"XP",.before = Commenced)
+
+#q2(c)
+arch0<-mutate(arch0,Arrows = str_match(arch0$RES, 'hit (\\d+) times from (\\d+) shots')[,2])
+arch0<-mutate(arch0,Targets = str_match(arch0$RES, 'hit (\\d+) times from (\\d+) shots')[,3])
+arch0<-mutate(arch0,RES = NULL)
+head(arch0,10)
+
